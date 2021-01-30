d411c7cf4751e8edb7717fc0cfe9613527bd34b9
Luis Saturno <saturno.luis@gmail.com>
2021-01-30 19:55:12 -0300

    Add base styles for Window

--- application/src/App.js
+++ application/src/App.js
@@ -2,9 +2,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>ventilador</h1>
-    </div>
+    <main className="App">
+      <div className="Window">
+        <header className="Window__header">
+          <div className="Window__title">
+            <div className="Window__controls">
+              <button className="Window__button">
+                <div className="Window__button--close" />
+              </button>
+              <button className="Window__button">
+                <div className="Window__button--minimize" />
+              </button>
+              <button className="Window__button">
+                <div className="Window__button--maximize" />
+              </button>
+            </div>
+          </div>
+        </header>
+      </div>
+    </main>
   );
 }
 
